36799b193e1a6d5d803b0e71574e23fd65155e07
simonegreco5 <simoxey.greco@gmail.com>
2026-04-16 00:11:16 +0200

    - 'AppFooter' add second section - add fix

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -33,6 +33,47 @@ export default function AppFooter(){
                     <a href="">dc power visa</a>
                 </div>
             </section>
+
+            {/* sezione info con logo in background */}
+            <section>
+                <div>
+                    <h3>DC COMICS</h3>
+                    <a href="">Characters</a>
+                    <a href="">Comics</a>
+                    <a href="">Movies</a>
+                    <a href="">TV</a>
+                    <a href="">Games</a>
+                    <a href="">Videos</a>
+                    <a href="">News</a>
+                </div>
+                <div>
+                    <h3>SHOP</h3>
+                    <a href="">Shop DC</a>
+                    <a href="">Shop DC Collect</a>
+                </div>
+                <div>
+                    <h3>DC</h3>
+                    <a href="">Term Of Use</a>
+                    <a href="">Privacy Policy</a>
+                    <a href="">Ad Choices</a>
+                    <a href="">Advertising</a>
+                    <a href="">Jobs</a>
+                    <a href="">Subscriptions</a>
+                    <a href="">Talent Workshops</a>
+                    <a href="">CPSC Certificates</a>
+                    <a href="">Ratings</a>
+                    <a href="">Shop Help</a>
+                    <a href="">Contact Us</a>
+                </div>
+                <div>
+                    <h3>SITES</h3>
+                    <a href="">DC</a>
+                    <a href="">MAD Magazine</a>
+                    <a href="">DC Kids</a>
+                    <a href="">DC Universe</a>
+                    <a href="">DC Power Visa</a>
+                </div>
+            </section>
         </footer>
     )
 }
