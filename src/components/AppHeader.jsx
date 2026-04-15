479f36c3ef9faf727065ea4973caf0184a011161
simonegreco5 <simoxey.greco@gmail.com>
2026-04-15 16:38:01 +0200

    'AppHeader' import logo and add navbar

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,6 +1,27 @@
+import AppLogo from '../assets/img/dc-logo.png'
 export default function AppHeader(){
 
     return(
-        <header>header</header>
+
+        <header>
+
+            {/* logo */}
+            <img src={AppLogo} alt="" />
+
+            {/* nav */}
+            <nav>
+                <a href="">characters</a>
+                <a href="">comics</a>
+                <a href="">movies</a>
+                <a href="">tv</a>
+                <a href="">games</a>
+                <a href="">collectibles</a>
+                <a href="">videos</a>
+                <a href="">fans</a>
+                <a href="">news</a>
+                <a href="">shop</a>
+            </nav>
+            
+        </header>
     )
 }
